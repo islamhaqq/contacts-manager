4a79474e698d2d87f39d6bf06382441098324411
Syed Aman <sna274@nyu.edu>
2017-10-30 11:01:39 -0500

    add some docs

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 import ListContacts from './components/ListContacts'
 import contacts from './utils/dummy-contacts-data'
 
+/**
+ * The root component of the app.
+ * @type {Component}
+ */
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* List of contacts. */}
         <ListContacts contacts={contacts} />
       </div>
     );
