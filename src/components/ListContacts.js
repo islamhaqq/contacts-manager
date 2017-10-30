1b3425766255644fc31d8c1df502205a02c05d5c
Syed Aman <sna274@nyu.edu>
2017-10-30 10:04:53 -0500

    create ListContacts component

---
+++ src/components/ListContacts.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+/**
+ * All contacts with their details listed out in an ordered list.
+ * @method ListContacts
+ * @param {Array} contacts - All the contacts to list out.
+ * @constructor
+ */
+function ListContacts ({contacts}) {
+  return (
+    <ol className="contact-list">
+      {this.props.contacts.map(contact => (
+        <li key={contact.id} className="contact-list-item">
+          {/* Avatar. */}
+          <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})` }} />
+
+          {/* Contact details. */}
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+
+          {/* Remove contact button. */}
+          <button className="contact-remove">Remove</button>
+        </li>
+      ))}
+    </ol>
+  )
+}
+
+export default ListContacts
