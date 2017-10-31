b7495ca98e91839cf75fbad289b94078f05f5382
Syed Aman <sna274@nyu.edu>
2017-10-31 05:56:56 -0500

    set contacts as initial state for App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,11 +8,24 @@ import contacts from '../utils/dummy-contacts-data'
  * @type {Component}
  */
 class App extends Component {
+  /**
+   * ESNext class field designated as component state.
+   * See TC39 proposal: https://github.com/tc39/proposal-class-fields.
+   * @type {Object}
+   */
+  state = {
+    /**rendered and listed outlisted and rendered out.
+     * All theArrayacts to be listed and rendered out.
+     * @type {Array}
+     */
+    contacts
+  }
+
   render() {
     return (
       <div className="App">
         {/* List of contacts. */}
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
