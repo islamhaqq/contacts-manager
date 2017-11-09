ca6e0898a8a4bbe66fe1372245bc6dc1ad3398c1
Syed Aman <sna274@nyu.edu>
2017-11-09 14:31:04 -0600

    notify components of browser url updates to later keep track of url changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './assets/styles/index.css';
 import App from './containers/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // notify component of url updates
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
