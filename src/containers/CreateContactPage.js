811f8c4b4802e6d054bc805fc69d9706bd2e3a46
Syed Aman <sna274@nyu.edu>
2017-11-09 16:06:11 -0600

    add content to create contact page

--- src/containers/CreateContactPage.js
+++ src/containers/CreateContactPage.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import ImageInput from './ImageInput';
 
 class CreateContactPage extends Component {
   render() {
-    return <h1>Create Contacts!</h1>;
+    return (
+      <div>
+        <Link to="/" className="close-create-contact" />
+
+        <form className="create-contact-form">
+          <ImageInput
+            name="avatarUrl"
+            className="create-contact-avatar-input"
+            maxHeight={64}
+          />
+          <div className="create-contact-details">
+            <input name="name" placeholder="Name" type="text" />
+            <input name="email" placeholder="Email" type="text" />
+
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
